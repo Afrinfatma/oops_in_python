72799da75ce4d02502a0e965b5876ef82d3fdf6d
Afrinfatma <afrinfatma4@gmail.com>
2022-09-08 16:29:46 +0530

    Added  Multilevel inheritance program

--- inheritance.py
+++ inheritance.py
@@ -1,18 +1,31 @@
 class College:
-    college_name= "Vinoba Bhave University"
-    college_wbsite="www.vbu.co.in"
+    def __init__(self,college_dict):
+        self.college_name=college_dict.get("college_name")
+        self.college_website=college_dict.get("college_website")
 
-    def get_info(self):
-        print("all your information about ",self.college_name ,"present on coIlege website ",self.college_wbsite)
 
 class Student(College):
-    def __init__(self, department_name):
-        self.department_name=department_name
+    def __init__(self, college_dict):
+        super().__init__(college_dict)
+        self.department_name=college_dict.get("department_name")
 
     def department_info(self):
         print("Student is studying in",self.department_name)
 
+class Shifts(Student):
+    def __init__(self,college_dict):
+        super().__init__(college_dict)
+        self.shift_type=college_dict.get("shift_type")
+
+
 if __name__=="__main__":
 
-    student_object=Student("Mca")
-    student_object.department_info()
+    # student_object=Student({"college_name":"Vbu","college_website":"www.vbu.nic.in","department_name":"MCA","shift_type":"Morning"})
+    # print(student_object)
+    # student_object.department_info()
+
+    student_object=Student({"college_name":"Vbu","college_website":"www.vbu.nic.in","department_name":"MCA","shift_type":"Morning"})
+    print(student_object.college_name)
+    print(student_object.college_website)
+    print(student_object.department_info())
+    print(student_object.department_name)
