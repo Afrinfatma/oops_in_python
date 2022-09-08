c1234a371bbce0c965d23f98e28042671df7c51d
Afrinfatma <afrinfatma4@gmail.com>
2022-09-08 17:20:26 +0530

    Added  Multiple inheritance program

--- inheritance.py
+++ inheritance.py
@@ -1,31 +1,28 @@
 class College:
-    def __init__(self,college_dict):
-        self.college_name=college_dict.get("college_name")
-        self.college_website=college_dict.get("college_website")
+    def __init__(self):
+        self.college_name="St.Columba's college"
+        self.college_website="www.columba's.co.in"
+    def get_info(self):
+        print("The college name is ",self.college_name,"and website is :",self.college_website)
 
+class Department:
+    def __init__(self):
+        self.department_name="MCA"
 
-class Student(College):
-    def __init__(self, college_dict):
-        super().__init__(college_dict)
-        self.department_name=college_dict.get("department_name")
+    def get_dept_info(self):
+        print("The department name is ",self.department_name)
 
-    def department_info(self):
-        print("Student is studying in",self.department_name)
+class Student(College,Department):
+    def __init__(self):
 
-class Shifts(Student):
-    def __init__(self,college_dict):
-        super().__init__(college_dict)
-        self.shift_type=college_dict.get("shift_type")
+        College.__init__(self)
+        Department.__init__(self)
 
+    def get_student_info(self):
+        print("The student is studying in : ", self.college_name)
+        print("The student is in department ", self.department_name)
+        print("The student can search on this link ", self.college_website)
 
-if __name__=="__main__":
-
-    # student_object=Student({"college_name":"Vbu","college_website":"www.vbu.nic.in","department_name":"MCA","shift_type":"Morning"})
-    # print(student_object)
-    # student_object.department_info()
-
-    student_object=Student({"college_name":"Vbu","college_website":"www.vbu.nic.in","department_name":"MCA","shift_type":"Morning"})
-    print(student_object.college_name)
-    print(student_object.college_website)
-    print(student_object.department_info())
-    print(student_object.department_name)
+student_object=Student()
+print(student_object.get_student_info())
+print(student_object.department_name)
