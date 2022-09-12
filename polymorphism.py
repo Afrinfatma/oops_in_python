a8c1ba77d21318ec101f5f03ed82c471cdf358bd
Afrinfatma <afrinfatma4@gmail.com>
2022-09-12 11:58:29 +0530

    Added  polymorphism and method overriding  program

---
+++ polymorphism.py
@@ -0,0 +1,16 @@
+
+if __name__ == '__main__':
+    # this section to use polymorphism using len function
+    print(len("Afrin_Fatma"))
+
+    print(len([1,3,5,7,9]))
+    print(len({1:"afrin",2:"Hashir",3:"Shakir"}))
+    # this section to show the polymorphism using + operator for 'string' as well as 'int'
+
+    num1=int(input("Enter num1:"))
+    num2=int(input("Enter num2:"))
+    print(num1+num2)
+
+    str1 = input("Enter string 1:")
+    str2 = input("Enter string 2:")
+    print(str1 + str2)
